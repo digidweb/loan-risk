1da1b64f50e7494ec6084e0e3325e1d9867732fb
digidweb <digid@digid.com.br>
2026-06-18 00:06:05 -0300

    Corrects routes for GET/loans/:id

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  resources :loans, only: [:index, :show, :create] do
-    collection do
-      get :concentration
-    end
+  scope :loans do
+    get 'concentration', to: 'loans#concentration'
   end
 
-  # Health check
-  get "/up", to: proc { [200, {}, [{ status: "ok" }.to_json]] }
+  resources :loans, only: %i[index show create]
+
+  get '/up', to: proc { [200, {}, [{ status: 'ok' }.to_json]] }
 end
